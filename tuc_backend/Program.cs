ad577c464834a7c3536e4704db959fb7a4cf9f82
Farid <farid-1234mn@hotmail.com>
2024-12-01 20:26:39 +0100

    skapat Sqlite-Database i backend och lagt CORS-policy

--- tuc_backend/Program.cs
+++ tuc_backend/Program.cs
@@ -1,3 +1,50 @@
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using tuc_backend.Data;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+//var app = builder.Build();
+
+//// Add Policy
+//builder.Services.AddCors(options => options.AddPolicy("DevelopmentApi",
+//    policy =>
+//    {
+//        policy.WithOrigins(builder.Configuration.GetValue<string>("AllowedHosts")!)
+//            .AllowAnyHeader()
+//            .AllowAnyMethod();
+//    })
+//);
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseCors("DevelopmentApi");
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using tuc_backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +53,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure database context
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add CORS policy
+builder.Services.AddCors(options => options.AddPolicy("DevelopmentApi",
+    policy =>
+    {
+        policy.WithOrigins(builder.Configuration.GetValue<string>("AllowedHosts")!)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    })
+);
 
 var app = builder.Build();
 
@@ -17,6 +78,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS policy
+app.UseCors("DevelopmentApi");
 
 app.UseAuthorization();
 
