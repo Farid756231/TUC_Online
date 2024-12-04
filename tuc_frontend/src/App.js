66e5ad140a79e939bfb19138e79425f1cc217e8d
KosBow <145974069+KosBow@users.noreply.github.com>
2024-12-04 09:50:03 +0100

    försöker lösa problemet med App.js

--- tuc_frontend/src/App.js
+++ tuc_frontend/src/App.js
@@ -1,20 +1,14 @@
-import './App.css';
-import Navbar from './components/navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home';
-import Programmering from './pages/produkter/programmering'
-import LoginSignup from './pages/LoginSignup/LoginSignup';
+import logo from "./logo.svg";
+import "./App.css";
+import LoginSignup from "./pages/LoginSignup/LoginSignup";
 
 function App() {
   return (
-  <Router>
-      <Routes>
-      <Route path="/" element={<Home/>} />
-         <Route path="/navbar" element={<Navbar/>} />
-         <Route path="/programmering" element={<Programmering />} />
-         <Route path='/login' element={<LoginSignup />} />
-      </Routes>
-  </Router> 
+    <div className="App">
+      <header className="App-header">
+        <LoginSignup />
+      </header>
+    </div>
   );
 }
 
